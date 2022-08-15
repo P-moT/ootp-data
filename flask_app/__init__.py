cf29baa9ec9f3a272b2c378d5461303b9407000c
P-moT <tommypeltier@gmail.com>
2022-08-15 00:13:46 -0400

    added sortable table JS. Changed log file behavior to start fresh with each run.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import logging
 
-logging.basicConfig(filename='record.log', level=logging.ERROR)
+logging.basicConfig(filename='record.log', level=logging.ERROR, filemode='w')
 app = Flask(__name__)
 app.secret_key = "1337"
 
